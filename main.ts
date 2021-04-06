47619fe5fd116881a878ecda0030c399eabebf8d
karelau <karelau@gmail.com>
2021-04-06 17:35:57 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,42 +1,22 @@
-gamePad.onEvent(GamerBitPin.P15, GamerBitEvent.Up, function () {
-    BitBuggy.brake()
-    radio.sendNumber(0)
-})
-gamePad.onEvent(GamerBitPin.P14, GamerBitEvent.Up, function () {
-    BitBuggy.brake()
-    radio.sendNumber(0)
-})
-gamePad.onEvent(GamerBitPin.P14, GamerBitEvent.Down, function () {
-    BitBuggy.turnleft()
-    radio.sendNumber(3)
-})
-gamePad.onEvent(GamerBitPin.P15, GamerBitEvent.Down, function () {
-    BitBuggy.turnright()
-    radio.sendNumber(4)
-})
-gamePad.onEvent(GamerBitPin.P13, GamerBitEvent.Down, function () {
-    BitBuggy.back()
-    radio.sendNumber(2)
-})
-gamePad.onEvent(GamerBitPin.P8, GamerBitEvent.Up, function () {
-    BitBuggy.brake()
-    radio.sendNumber(0)
-})
-gamePad.onEvent(GamerBitPin.P8, GamerBitEvent.Down, function () {
-    BitBuggy.forward()
-    radio.sendNumber(1)
-})
-gamePad.onEvent(GamerBitPin.P13, GamerBitEvent.Up, function () {
-    BitBuggy.brake()
-    radio.sendNumber(0)
+bluetooth.onBluetoothConnected(function () {
+    basic.showLeds(`
+        # # # # .
+        # # . . .
+        # . # . .
+        # . . # .
+        . . . . #
+        `)
+    RingbitCar.running_time(RingbitCar.Direction_run.forward, 2)
 })
 basic.showLeds(`
-    # # # # .
-    # # . . .
-    # . # . .
-    # . . # .
-    . . . . #
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
     `)
-radio.setGroup(1)
-BitBuggy.init_wheel(AnalogPin.P1, AnalogPin.P2)
-BitBuggy.freestyle(0, 0)
+basic.forever(function () {
+    while (true) {
+    	
+    }
+})
